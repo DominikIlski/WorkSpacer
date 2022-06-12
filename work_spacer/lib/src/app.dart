1e3f4549ecc2eee02bda6020f556cf4290cf13c9
msroczek <maciej.sroczek@gmail.com>
2022-06-12 14:08:16 +0200

    Prepared localizations support. Use global variable singleton 'translate' to access translation strings. Whenever new elements are added, run 'flutter gen-l10n' in terminal in order to re-generate AppLocalizations.

--- work_spacer/lib/src/app.dart
+++ work_spacer/lib/src/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
+import 'package:work_spacer/i18n.dart';
 import 'package:work_spacer/screens/admin/block/workspace_block_screen.dart';
 import 'package:work_spacer/screens/admin/cancel/reservation_cancel_screen.dart';
 import 'package:work_spacer/screens/admin/roles/role_management_screen.dart';
@@ -74,22 +75,22 @@ class MyApp extends StatelessWidget {
             // allows descendant Widgets to display the correct translations
             // depending on the user's locale.
             localizationsDelegates: const [
-              AppLocalizations.delegate,
+              ...AppLocalizations.localizationsDelegates,
               GlobalMaterialLocalizations.delegate,
               GlobalWidgetsLocalizations.delegate,
               GlobalCupertinoLocalizations.delegate,
             ],
-            supportedLocales: const [
-              Locale('en', ''), // English, no country code
-            ],
+            supportedLocales: AppLocalizations.supportedLocales,
 
             // Use AppLocalizations to configure the correct application title
             // depending on the user's locale.
             //
             // The appTitle is defined in .arb files found in the localization
             // directory.
-            onGenerateTitle: (BuildContext context) =>
-                AppLocalizations.of(context)!.appTitle,
+            onGenerateTitle: (BuildContext context) {
+              translate = AppLocalizations.of(context)!;
+              return translate.appTitle;
+            },
 
             // Define a light and dark color theme. Then, read the user's
             // preferred ThemeMode (light, dark, or system default) from the
