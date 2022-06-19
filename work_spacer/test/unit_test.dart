d4e78247f80c85e4cf84e14993839efaebb15225
Sebastian Smolarczyk <sebastian.smolarczyk@gmail.com>
2022-06-19 02:31:17 +0200

    Update unit_test.dart

--- work_spacer/test/unit_test.dart
+++ work_spacer/test/unit_test.dart
@@ -7,9 +7,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:work_spacer/models/desk.dart';
 import 'package:work_spacer/models/filter.dart';
+import 'package:work_spacer/models/user.dart';
 import 'package:work_spacer/stores/block_store.dart';
 import 'package:work_spacer/stores/desks_store.dart';
 import 'package:work_spacer/stores/reservation_store.dart';
+import 'package:work_spacer/stores/role_management_store.dart';
+import 'package:work_spacer/stores/rooms_store.dart';
 
 void main() {
   group('Desks tests', () {
@@ -38,10 +41,45 @@ void main() {
       final desksStore = DesksStore();
       await desksStore.fetchDesks();
       desksStore.filterStore.toggleSimpleFilter(FilterParameter.secondMonitor);
-      var isTrue = desksStore.desks.every((element) => element.secondMonitor == true);
+      var isTrue = desksStore.desks.every((element) =>
+      element.secondMonitor == true);
       expect(isTrue, true);
     });
   });
+
+  group('Room tests', () {
+    test('fetch', () async {
+      final roomsStore = RoomsStore();
+      await roomsStore.fetchRooms();
+
+      expect(roomsStore.rooms.isNotEmpty, true);
+    });
+    test('filter by capacity >20', () async {
+      final roomsStore = RoomsStore();
+      await roomsStore.fetchRooms();
+      roomsStore.filterStore.toggleValueFilter(FilterParameter.capacity, '21');
+      var isTrue = roomsStore.rooms.every((element) => element.capacity > 20);
+      expect(isTrue, true);
+    });
+    test('floor filter remove', () async {
+      final roomsStore = RoomsStore();
+      await roomsStore.fetchRooms();
+      int initialListLength = roomsStore.rooms.length;
+      roomsStore.filterStore.toggleValueFilter(FilterParameter.floor, '1');
+      roomsStore.filterStore.resetFilter(FilterParameter.floor);
+      expect(roomsStore.rooms.length, initialListLength);
+    });
+    test('filter by projector', () async {
+      final roomsStore = RoomsStore();
+      await roomsStore.fetchRooms();
+      roomsStore.filterStore.toggleSimpleFilter(FilterParameter.projector);
+      var isTrue = roomsStore.rooms.every((element) =>
+      element.hasProjector == true);
+      expect(isTrue, true);
+    });
+  });
+
+
   group('reservation tests', () {
     test('fetch', () async {
       final deskResStore = ReservationStore();
@@ -54,7 +92,29 @@ void main() {
       await deskResStore.fetchReservations();
       int initialListLength = deskResStore.reservations.length;
       deskResStore.cancel(deskResStore.reservations.first);
-      expect(deskResStore.reservations.length, initialListLength-1);
+      expect(deskResStore.reservations.length, initialListLength - 1);
     });
   });
-}
+
+    group('user tests', () {
+      test('fetch', () async {
+        final roleMagStore = RoleManagementStore();
+        await roleMagStore.fetchUsers();
+
+        expect(roleMagStore.users.isNotEmpty, true);
+      });
+      test('change role', () async {
+        final roleMagStore = RoleManagementStore();
+        await roleMagStore.fetchUsers();
+        int index = 0;
+        if (roleMagStore.users[index].role == Role.senior) {
+          index++;
+        }
+        else {
+          roleMagStore.users[index].setRole(Role.senior);
+        }
+
+        expect(roleMagStore.users[index].role, Role.senior);
+      });
+    });
+  }
